5fb5100b888a8adacb47c5e6cea3b1d1532c8ee3
Harms <543232@bah.com>
2019-01-01 17:43:36 -0500

    Configuring 'Verify' Function

--- server/app.js
+++ server/app.js
@@ -13,6 +13,27 @@ app.use(bodyParser.urlencoded({ extended : false}))
 app.get('/api/user/login', (req, res) => {
     res.send('Hello World!')
 })
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+
+  // Find the user by username.  If there is no user with the given
+  // username, or the password is not correct, set the user to `false` to
+  // indicate failure and set a flash message.  Otherwise, return the
+  // authenticated `user`.
+
+  findByUsername(username, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) { 
+          return done(null, false, { message: 'Unknown user ' + username }); 
+      }
+      if (user.password != password) { 
+          return done(null, false, { message: 'Invalid password' }); 
+      }
+        return done(null, user);
+      })
+  }
+));
  
 app.post('/api/user/login', (req, res) => {
     mongoose.connect(url,{ useMongoClient: true }, function(err){
